43620e14b20ecf27efcd036c6fe6cd1c4cf825fd
Lisa Paladin <lisa.paladin@gmail.com>
2018-07-09 22:13:04 -0400

    Fix restart button and shuffle cards

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Master (home)
 // STATUS: Cards flipping and matching! 
-// FIXED not shuffling; stopped flipping after adding shuffle and moves counter
+// not shuffling
 // moves counter not counting
 // too many cards can flip at once; also counts closing clicks
+// clicking same card twice yields a match
 // lightning bolts won't change color / match; fix this
 // anchors remain open; need to close
 // no modal yet
@@ -13,33 +14,29 @@
 let cards = document.querySelectorAll('.card');
 const openCards = [];
 let restart = document.querySelector('.restart');
+const moves = document.querySelector('moves');
+const deck = document.querySelector('.deck');
 
 let displayCard = function () {
   this.classList.toggle('open');
   this.classList.toggle('show');
 
-
-// NEW STARTS HERE 7/8
-
-
-  // add the clicked card with .open class to a new array called openCards
+  // WORKS / add the clicked card with .open class to a new array called openCards
 openCards.push(this);
 
-  // check if the new array contains two items 
-  // if it does, call back a checker function
+  // WORKS / check if the new array contains two items 
+  // WORKS / if it does, call back a checker function
 if (openCards.length === 2) {
 	console.log('two!');
 	checkMatch();
 };
 }
 
-// ** (Lisa) NEW STARTS HERE 7/5
-
-// check if two cards in new array are a match
+// WORKS / check if two cards in new array are a match
 function checkMatch () {
   if (openCards[0].innerHTML ===
      openCards[1].innerHTML) {
-  	// loop over cards in openCards array; add match class
+  	// WORKS / loop over cards in openCards array; add match class
 		for (card of openCards) { 
 			card.classList.add('match');
 	  		}
@@ -47,42 +44,30 @@ function checkMatch () {
     	openCards.length = 0; // empty array after matching
   }
   else {
-  	// loop over cards; flip them back over if no match
-    	openCards.length = 0; // empty array after matching
+  	// WORKS / loop over cards; flip them back over if no match
+    	openCards.length = 0; // WORKS / empty array after matching
     console.log('no match :( ')
   }
 }
 
-
-// As in: if the two cards match, toggle a 'match' class (which you have to make in css) else, toggle a 'wrong' class (you also have to make this). 
-// At the end of the function (after the if/else), empty out the array
-
-
-// NEW STARTS HERE 7/8
-// start game; this isn't working yet; added restart variable above
+// NOT WORKING / start game; added restart variable above
 let restartGame = function () {
-		shuffle(cards); 
+		//shuffle(cards); 
 		//scorePanel
 		moves = 0;
-	}
+}
 
-
-// add event listener to restart button; call startGame function
+// NOT WORKING / add event listener to restart button; call startGame function
 restart.addEventListener('click', restartGame);
 
-
-//
 // ======================================== END OF FIRST SESSION =======================================
 
-
-
-
-// loop to add event listeners
+// WORKS / loop to add event listeners
 for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', displayCard);
 }
 
-// Shuffle function from http://stackoverflow.com/a/2450976, provided by Udacity in starter code
+// NOT WORKING / Shuffle function from http://stackoverflow.com/a/2450976, provided by Udacity in starter code
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -94,6 +79,10 @@ function shuffle(array) {
         array[randomIndex] = temporaryValue;
     }
 
+    // NOT WORKING / write a for loop that appends cards to the deck using the deck and the array
+    for (card of cards) {
+    	deck.appendChild(card);
+    }
     return array;
 }
 
